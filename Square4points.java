283ed27024e37a75788e3d12bff12befe6fbc21e
PriyaSagunth <priya.s1313087@ksrce.ac.in>
2016-08-06 21:17:11 -0700

    Update Square4points.java

--- Square4points.java
+++ Square4points.java
@@ -2,10 +2,11 @@
 	import java.util.Scanner;
 	public class Square4points {
 	    public static void main(String[] args) {
-	        Scanner sc=new Scanner(System.in);int[] a=new int[8];
-	        System.out.println("enter 4 points as (x,y) coordinates");
+	        Scanner s=new Scanner(System.in);
+	        int[] a=new int[8];
+	        System.out.println("enter your 4 points as (x,y) coordinates");
 	        for(int i=0;i<8;i++)
-	            a[i]=sc.nextInt();
+	            a[i]=s.nextInt();
 	        if(a[0]==a[2]&&a[4]==a[6]&&a[1]==a[7]&&a[3]==a[5])
 	                    System.out.println("this coodinates can form square");
 	        else
